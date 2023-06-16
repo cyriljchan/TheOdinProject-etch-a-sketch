bfa97d9a3c238aaa54ddfd9fffd48d175d315b12
Cyril Jay L. Chan <cyriljchan@yahoo.com>
2023-06-16 16:02:03 +0800

    draw with mouse hover

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+function hoverColor() {
+    document.querySelectorAll('.grid').forEach(
+        grid => grid.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = 'black';
+        })
+    )
+}
+
 const container = document.createElement('div');
 container.classList.add('grid-board');
 document.body.appendChild(container);
-
 let gridSize = prompt("Please enter the desired grid size.");
+
 for (let i = 0; i < gridSize; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
@@ -11,5 +19,6 @@ for (let i = 0; i < gridSize; i++) {
         grid.classList.add('grid');
         row.appendChild(grid);
     }
-    container.appendChild(row);
+    document.querySelector('.grid-board').appendChild(row);
 }
+hoverColor();
