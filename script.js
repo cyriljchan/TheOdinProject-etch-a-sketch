743d81af6820ad253358a76e7d5da0308cdc5a7e
Cyril Jay L. Chan <cyriljchan@yahoo.com>
2023-06-16 16:16:01 +0800

    buttons for grid size and clearing grid

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function createGrid() {
+    document.querySelector('.grid-board').textContent = '';
+    let gridSize = prompt("Please enter the desired grid size.");
+    if (gridSize > 100) {alert("Please enter a number not more than 100."); return}
+
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let i = 0; i < gridSize; i++) {
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            row.appendChild(grid);
+        }
+        document.querySelector('.grid-board').appendChild(row);
+    }
+    hoverColor();
+}
 function hoverColor() {
     document.querySelectorAll('.grid').forEach(
         grid => grid.addEventListener('mouseover', (e) => {
@@ -6,19 +23,22 @@ function hoverColor() {
     )
 }
 
+function resetColor() {
+    document.querySelectorAll('.grid').forEach(
+        grid => grid.style.backgroundColor = "transparent"
+    )
+}
+
+
 const container = document.createElement('div');
 container.classList.add('grid-board');
 document.body.appendChild(container);
-let gridSize = prompt("Please enter the desired grid size.");
 
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for (let i = 0; i < gridSize; i++) {
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
-        row.appendChild(grid);
-    }
-    document.querySelector('.grid-board').appendChild(row);
-}
-hoverColor();
+document.querySelector('button.createGrid').addEventListener('click', () => {
+    createGrid();
+    console.log("create grid");
+})
+document.querySelector('button.clear').addEventListener('click', () => {
+    resetColor();
+    console.log("clear grid");
+})
