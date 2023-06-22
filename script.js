667b8f6529b18a2c5de39896ef4ca2768ebb2cc6
Cyril Jay L. Chan <cyriljchan@yahoo.com>
2023-06-22 15:50:50 +0800

    added draw styles

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
-function createGrid() {
+
+
+function drawMarker(e){
+    e.target.style.backgroundColor = `black`;
+}
+
+function drawPencil(e){
+    let bgcolor = window.getComputedStyle(e.target).backgroundColor;
+    let rgba = bgcolor.substring(bgcolor.indexOf('(') +1, bgcolor.length -1).split(', ');
+    if (rgba[3] === undefined) {
+        e.target.style.backgroundColor = `rgb(${+rgba[0] - 23}, ${+rgba[1] - 23}, ${+rgba[2] - 23})`;
+    }
+    else {
+        e.target.style.backgroundColor = `rgba(${+rgba[0] - 23}, ${+rgba[1] - 23}, ${+rgba[2] - 23}, ${+rgba[3] + 0.1})`;
+    }
+}
+
+function generateRandomRGBA(){
+    let r = Math.random() * 256;
+    let g = Math.random() * 256;
+    let b = Math.random() * 256;
+    let a = Math.random();
+
+    return [r, g, b, a]
+}
+function drawRainbow(e){
+    let rgba = generateRandomRGBA();
+    e.target.style.backgroundColor = `rgba(${rgba[0]}, ${rgba[1]}, ${rgba[2]}, ${+rgba[3]})`;
+}
+
+function hoverDraw(){
+    document.querySelectorAll('.grid').forEach(
+        grid => grid.addEventListener('mouseover', (e) => {
+            if (drawStyle === 'Marker') drawMarker(e);
+            else if (drawStyle === 'Pencil') drawPencil(e);
+            else if (drawStyle === 'Rainbow') drawRainbow(e);
+            else marker(e);
+        })
+    )
+}
+
+function resetColor() {
+    document.querySelectorAll('.grid').forEach(
+        grid => grid.style.backgroundColor = "transparent"
+    )
+}
+
+function setGrid(size) {
     document.querySelector('.grid-board').textContent = '';
-    let gridSize = prompt("Please enter the desired grid size.");
+    let gridSize = (size==undefined) ? prompt("Please enter the desired grid size.") : 16;
     if (gridSize > 100) {alert("Please enter a number not more than 100."); return}
 
     for (let i = 0; i < gridSize; i++) {
@@ -13,32 +60,19 @@ function createGrid() {
         }
         document.querySelector('.grid-board').appendChild(row);
     }
-    hoverColor();
+    hoverDraw();
 }
-function hoverColor() {
-    document.querySelectorAll('.grid').forEach(
-        grid => grid.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = 'black';
-        })
-    )
-}
-
-function resetColor() {
-    document.querySelectorAll('.grid').forEach(
-        grid => grid.style.backgroundColor = "transparent"
-    )
-}
-
 
 const container = document.createElement('div');
 container.classList.add('grid-board');
 document.body.appendChild(container);
+setGrid(16);
+let drawStyle = 'Marker';
 
-document.querySelector('button.createGrid').addEventListener('click', () => {
-    createGrid();
-    console.log("create grid");
-})
-document.querySelector('button.clear').addEventListener('click', () => {
-    resetColor();
-    console.log("clear grid");
-})
+document.querySelector('button.setGrid').addEventListener('click', () => {setGrid()});
+document.querySelector('button.clear').addEventListener('click', () => {resetColor()});
+document.querySelectorAll('button.drawStyle').forEach(
+    button => button.addEventListener('click', (e) => {
+        drawStyle = e.target.textContent;
+    })
+)
